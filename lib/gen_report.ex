92c00474452fe53b1ce54fda12450088a159f7be
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2022-05-14 20:39:50 -0300

    Done

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,77 @@
 defmodule GenReport do
+  alias GenReport.Parser
+
+  @freelancers [
+    "cleiton",
+    "daniele",
+    "danilo",
+    "diego",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "mayk",
+    "rafael",
+    "vinicius"
+  ]
 
   def build do
-    # TO DO
+    {:error, "Insira o nome de um arquivo"}
+  end
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), &sum_hours/2)
+  end
+
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Informe uma lista de arquivos"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> merge_maps(report, result) end)
+  end
+
+  defp report_acc() do
+    months = Enum.into(Map.values(Parser.get_months()), %{}, &{&1, 0})
+    years = Enum.into(2016..2020, %{}, &{&1, 0})
+
+    %{
+      "all_hours" => Enum.into(@freelancers, %{}, &{&1, 0}),
+      "hours_per_month" => Enum.into(@freelancers, %{}, &{&1, months}),
+      "hours_per_year" => Enum.into(@freelancers, %{}, &{&1, years})
+    }
+  end
+
+  defp sum_hours(
+         [name, hours, _day, month, year],
+         %{
+           "all_hours" => all_hours,
+           "hours_per_month" => hours_per_month,
+           "hours_per_year" => hours_per_year
+         }
+       ) do
+    user_monthly = sum_sub_values(hours_per_month[name], month, hours)
+    user_yearly = sum_sub_values(hours_per_year[name], year, hours)
+
+    %{
+      "all_hours" => Map.put(all_hours, name, all_hours[name] + hours),
+      "hours_per_month" => Map.put(hours_per_month, name, user_monthly),
+      "hours_per_year" => Map.put(hours_per_year, name, user_yearly)
+    }
+  end
+
+  defp sum_sub_values(name, key, hours) do
+    Map.put(name, key, name[key] + hours)
+  end
+
+  defp merge_maps(map_1, map_2) when is_map(map_1) do
+    Map.merge(map_1, map_2, fn _k, v1, v2 -> merge_maps(v1, v2) end)
+  end
+
+  defp merge_maps(v1, v2) do
+    v1 + v2
   end
 end
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -6,6 +6,8 @@ defmodule GenReportTest do
 
   @file_name "gen_report.csv"
 
+  @file_names ["reports/part_1.csv", "reports/part_2.csv", "reports/part_3.csv"]
+
   describe "build/1" do
     test "When passing file name return a report" do
       response = GenReport.build(@file_name)
@@ -17,6 +19,20 @@ defmodule GenReportTest do
       response = GenReport.build()
 
       assert response == {:error, "Insira o nome de um arquivo"}
+    end
+  end
+
+  describe "build_from_many/1" do
+    test "When passing a list of file names, return a report" do
+      response = GenReport.build_from_many(@file_names)
+
+      assert response == ReportFixture.build()
+    end
+
+    test "When no list of file names was given, returns an error" do
+      response = GenReport.build_from_many("nolist")
+
+      assert response == {:error, "Informe uma lista de arquivos"}
     end
   end
 end
